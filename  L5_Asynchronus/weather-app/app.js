703f4c04e38062222b4643f0e265d8dc8df08f5b
Kevin Huang <kevin.huang@presslogic.com>
2022-03-22 16:25:52 +0800

    Slgihtly fix the end of L6S10

---  L5_Asynchronus/weather-app/app.js
+++  L5_Asynchronus/weather-app/app.js
@@ -1,33 +1,14 @@
-const request = require('request');
+
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
 
 
-// request({url: url, json: true}, (error, response) =>{
-//    console.log("It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out.");
-    
-// })  
-
-//Geocoding 
-//Address -> Lat/Long -> Weather
-
-//
-//Goal:Create a reusable function for getting the forecast
-//
-//1. Setups the "forecast" function in utils/forecast.js
-//2. Require the function in appp.js and cal it as shown below
-//3. The forecst function should have three potential calls to callback:
-//  - Low level error, pass string for error
-//  - Coordinate error, pass string for error
-//  - Success, pass forecast string for data (same foramt as from before)
-
-
  
-// geocode('Philadelphia New york', (error, data) => {
-//     console.log('Error', error);
-//     console.log('Data', data);
+geocode('Philadelphia New york', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
 
-// })
+})
 
 
 forecast(37.8267, -122.4233, (error, data) =>{
