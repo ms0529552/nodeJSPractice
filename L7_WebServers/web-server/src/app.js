89acdc2200821e3250b5eb75dbd762ddd6665e87
Kevin Huang <kevin.huang@presslogic.com>
2022-06-15 11:03:16 +0800

    L7-Serving up HTML and JSON done

--- L7_WebServers/web-server/src/app.js
+++ L7_WebServers/web-server/src/app.js
@@ -16,18 +16,28 @@ app.get('/help', (req, res)=> {
 //1. Setup an about route and render a page title.
 //2. Set up an weather route and render a page title.
 //3. Test your work by visiting both in the browser
+//
+//2nd Goal: Update routes
+//1. Setup about route to render a title with HTML
+//2. Setup a weather route to send back JSon
+//  - Object with forecast and location strings
+//3. Test your work by visiting both in the browser
+
 //====================================================================
 
 app.get('/about', (req, res) =>{
-    res.send('About us.');
-})
+    res.send('<h1>About us</h1>');
+});
  
 app.get('/weather', (req, res) =>{
-    res.send('Current weather status');
-})
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia',
+    });
+});
 
 //====================================================================
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
-})
+});
